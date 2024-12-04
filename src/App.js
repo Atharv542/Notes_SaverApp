9d9d4ae470c6c68b8f71247145cd3db2787e4937
Atharv542 <atharvragdwal05@gmail.com>
2024-12-04 19:11:08 +0530

    Initial Setup Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Pastes from './components/Pastes';
+import ViewPate from './components/ViewPate';
 
 function App() {
+  const router= createBrowserRouter(
+    [
+      {
+        path:'/',
+        element:
+        <div>
+          <Navbar/>
+          <Home/>
+        </div>
+      },
+      {
+        path:'/pastes',
+        element:
+        <div>
+            <Navbar/>
+            <Pastes/>
+          
+        </div>
+      },
+      {
+        path:'/pastes/:id',
+        element:
+        <div>
+          <Navbar/>
+          <ViewPate/>
+        </div>
+      },
+    ]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <RouterProvider router={router}/>
     </div>
   );
 }
